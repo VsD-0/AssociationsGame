3306db28c648dc3e97d564f81849f12adc4939cc
Дмитрий Васильев <dimvas13@mail.ru>
2022-05-31 11:31:37 +0500

    Добавьте файлы проекта.

---
+++ AssociationsGame/GamePage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace AssociationsGame
+{
+    /// <summary>
+    /// Логика взаимодействия для GamePage.xaml
+    /// </summary>
+    public partial class GamePage : Page
+    {
+        List<TextBoxClass> letter = new List<TextBoxClass>();
+        public GamePage()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 5; i++)
+            {
+                TextBoxClass tbc = new TextBoxClass();
+                letter.Add(tbc);
+            }
+            
+            Users.ItemsSource = letter;
+        }
+
+        private void textBoxLetter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string word;
+            foreach (var item in letter)
+            {
+                word += item.TbContent
+            }
+        }
+    }
+    public class TextBoxClass
+    {
+        public string TbContent { get; set; }
+    }
+}
---
+++ AssociationsGame/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AssociationsGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CloseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void MinCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaxCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void MaxCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
